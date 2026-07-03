07f735ff7fc59e65f143682a3920b224f802c234
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-03 01:34:19 +0100

    Add initial project structure with Maven configuration and application files

---
+++ src/main/java/hosting_support_backend/HostingSupportBackendApplication.java
@@ -0,0 +1,13 @@
+package hosting_support_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingSupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostingSupportBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hosting_support_backend/HostingSupportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hosting_support_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostingSupportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
