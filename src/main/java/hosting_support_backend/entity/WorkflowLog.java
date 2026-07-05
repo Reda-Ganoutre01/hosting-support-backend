d26822f01ea183da50bfbc8453207358e0a478ae
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-05 17:11:10 +0100

    feat: add WorkflowLog entity with fields for id, workflowName, executionStatus, and executionDate

--- src/main/java/hosting_support_backend/entity/WorkflowLog.java
+++ src/main/java/hosting_support_backend/entity/WorkflowLog.java
@@ -1,4 +1,14 @@
 package hosting_support_backend.entity;
 
+
+import jakarta.persistence.Entity;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class WorkflowLog {
+    long id;
+    String workflowName;
+    String executionStatus;
+    LocalDateTime executionDate;
 }
