91acb392ebffc4930e6f0fface82d8fa1779a4b0
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-06 15:46:02 +0100

    feat: enhance User entity with Lombok annotations and formatting improvements

--- src/main/java/hosting_support_backend/entity/User.java
+++ src/main/java/hosting_support_backend/entity/User.java
@@ -1,18 +1,37 @@
 package hosting_support_backend.entity;
 
 import hosting_support_backend.entity.enums.Role;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+
+
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
+
     String firstName;
+
     String lastName;
+    @Column(unique = true)
     String email;
+
     String password;
+
     String phone;
+
+
+    @Enumerated(EnumType.STRING)
     Role role;
     Boolean enabled;
     LocalDateTime createdAt;
