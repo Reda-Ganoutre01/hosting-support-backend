d931aceb0851d52a64823b43034985fe3cd16af4
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-06 15:28:58 +0100

    feat: update UserRepository to find user by ID and add MessageRepository for user messages

--- src/main/java/hosting_support_backend/repository/MessageRepository.java
+++ src/main/java/hosting_support_backend/repository/MessageRepository.java
@@ -1,11 +1,14 @@
 package hosting_support_backend.repository;
 
 
+import hosting_support_backend.entity.Message;
 import hosting_support_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(long id);
+public interface MessageRepository extends JpaRepository<Message ,Long> {
+    List<Message> findByUser(User user);
 }
