ef10a812a8411b21e9b5be88f35c87fb1fda67a9
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-06 15:12:25 +0100

    feat: create UserRepository class

---
+++ src/main/java/hosting_support_backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package hosting_support_backend.repository;
+
+
+import hosting_support_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
