a077b4e9029f54030af0cacefe83e53e4ae1932f
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-06 15:58:37 +0100

    feat: enhance WorkflowLog entity with Lombok annotations and formatting improvements

--- src/main/java/hosting_support_backend/entity/WorkflowLog.java
+++ src/main/java/hosting_support_backend/entity/WorkflowLog.java
@@ -1,14 +1,30 @@
 package hosting_support_backend.entity;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table( name ="workflow_logs")
+
 public class WorkflowLog {
+    @Id
+            @GeneratedValue( strategy = GenerationType.IDENTITY)
     long id;
+
+    @Column (nullable = false)
     String workflowName;
+
+    @Column (nullable = false)
     String executionStatus;
+
+    @Column(nullable = false)
     LocalDateTime executionDate;
 }
