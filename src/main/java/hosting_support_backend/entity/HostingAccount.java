94774a76c4ee19acbd213e4486d21c5e53aa0209
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-07 15:46:34 +0100

    feat: enhance HostingAccount entity with Lombok annotations and formatting improvements

--- src/main/java/hosting_support_backend/entity/HostingAccount.java
+++ src/main/java/hosting_support_backend/entity/HostingAccount.java
@@ -2,14 +2,25 @@ package hosting_support_backend.entity;
 
 
 import hosting_support_backend.entity.enums.HostingStatus;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table ( name = "hosting_accounts")
 public class HostingAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false)
     String domainName;
     HostingStatus status;
     LocalDate startDate;
