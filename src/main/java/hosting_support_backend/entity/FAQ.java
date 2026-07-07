56703b05f9a9d6c3600d5c5a6912909821a3946e
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-07 16:25:28 +0100

    feat: refactor FAQ entity to improve field visibility and structure

--- src/main/java/hosting_support_backend/entity/FAQ.java
+++ src/main/java/hosting_support_backend/entity/FAQ.java
@@ -1,11 +1,31 @@
 package hosting_support_backend.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FAQ {
-  Long id;
-  String question;
-  String answer;
-  String category;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(length = 1000, nullable = false)
+  private String question;
+  @Column(nullable = false, length = 4000)
+
+  private String answer;
+  private String category;
 }
