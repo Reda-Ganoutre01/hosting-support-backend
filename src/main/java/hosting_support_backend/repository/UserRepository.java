48beaefe8f41d8abe79b1b17a90007b094ebacc4
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-07 15:47:54 +0100

    feat: update UserRepository to include email-based user retrieval methods

--- src/main/java/hosting_support_backend/repository/UserRepository.java
+++ src/main/java/hosting_support_backend/repository/UserRepository.java
@@ -5,7 +5,10 @@ import hosting_support_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(long id);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail (String email);
 }
