5b4a17ecc6720fe7c50383d4076e5b39a18a5e4b
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-08 16:47:06 +0100

    feat: implement HostingAccountRepository interface extending JpaRepository for CRUD operations

--- src/main/java/hosting_support_backend/repository/HostingAccountRepository.java
+++ src/main/java/hosting_support_backend/repository/HostingAccountRepository.java
@@ -1,8 +1,18 @@
 package hosting_support_backend.repository;
 
 
+import hosting_support_backend.entity.HostingAccount;
+import hosting_support_backend.entity.enums.HostingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface HostingAccountRepository {
+public interface HostingAccountRepository extends JpaRepository<HostingAccount,Long> {
+    List<HostingAccount> findByUserId(Long userId);
+    List<HostingAccount> findByStatus(HostingStatus status);
+    Optional<HostingAccount> findByDomainName(String domainName);
+    boolean existsByDomainName(String domainName);
 }
