6fa8121b491ac5c1e24c7f160d797ce8d7b8bb12
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-08 16:36:50 +0100

    feat: add method to find messages by ticket ID in MessageRepository

--- src/main/java/hosting_support_backend/repository/MessageRepository.java
+++ src/main/java/hosting_support_backend/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package hosting_support_backend.repository;
 
 
 import hosting_support_backend.entity.Message;
+import hosting_support_backend.entity.Ticket;
 import hosting_support_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message ,Long> {
     List<Message> findByUser(User user);
+    List<Message> findByTicketId(long TicketId);
 }
