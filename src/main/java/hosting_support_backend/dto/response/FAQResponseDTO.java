d3bc5f98ed333668e72133d1f5ad1c42258f098a
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-13 17:44:16 +0100

    feat: create FAQResponseDTO class for FAQ response data structure

---
+++ src/main/java/hosting_support_backend/dto/response/FAQResponseDTO.java
@@ -0,0 +1,9 @@
+package hosting_support_backend.dto.response;
+
+public class FAQResponseDTO {
+    private Long id;
+    private String question;
+    private String answer;
+    private String category;
+
+}
