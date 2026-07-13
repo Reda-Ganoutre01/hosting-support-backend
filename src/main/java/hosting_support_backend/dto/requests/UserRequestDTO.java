c814d73894cb9befcb1bf291497aeee37d7689e9
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-13 17:31:40 +0100

    feat: add UserRequestDTO class for user input data structure

---
+++ src/main/java/hosting_support_backend/dto/requests/UserRequestDTO.java
@@ -0,0 +1,29 @@
+package hosting_support_backend.dto.requests;
+
+import hosting_support_backend.entity.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class UserRequestDTO {
+
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    private String userName;
+
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String phone;
+
+    @NotNull
+    private Role role;
+}
