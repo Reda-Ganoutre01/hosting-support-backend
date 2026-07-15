ddec1fe5edadb5f1b27d38d87e94e17678e46c74
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 00:58:37 +0100

    fix: add @JsonIgnore annotation to password and list fields in User entity

--- src/main/java/hosting_support_backend/entity/User.java
+++ src/main/java/hosting_support_backend/entity/User.java
@@ -4,6 +4,7 @@ import hosting_support_backend.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 
     private String phone;
@@ -44,12 +46,15 @@ public class User {
 
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Ticket> tickets;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<HostingAccount> hostingAccounts;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Notification> notifications;
 
     @PrePersist
