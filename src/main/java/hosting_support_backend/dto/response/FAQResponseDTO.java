4fd0d920445ae127e4d17751fd290a352d2d7c82
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 19:20:28 +0100

    feat: add Lombok annotations to FAQResponseDTO for boilerplate reduction

--- src/main/java/hosting_support_backend/dto/response/FAQResponseDTO.java
+++ src/main/java/hosting_support_backend/dto/response/FAQResponseDTO.java
@@ -1,5 +1,14 @@
 package hosting_support_backend.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FAQResponseDTO {
     private Long id;
     private String question;
