17005bc6ccb1b72fd0e803e4c0864a19179057b5
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 18:55:10 +0100

    feat: add Lombok annotations to HostingPlanResponseDTO for boilerplate reduction

--- src/main/java/hosting_support_backend/dto/response/HostingPlanResponseDTO.java
+++ src/main/java/hosting_support_backend/dto/response/HostingPlanResponseDTO.java
@@ -1,5 +1,15 @@
 package hosting_support_backend.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HostingPlanResponseDTO {
   
     private Long id;
