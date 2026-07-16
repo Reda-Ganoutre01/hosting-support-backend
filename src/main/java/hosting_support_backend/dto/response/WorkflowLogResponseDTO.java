181c0e1bb73b119f69f7dda6d1dce2b11673bef9
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 19:20:00 +0100

    feat: add Lombok annotations to WorkflowLogResponseDTO for boilerplate reduction

--- src/main/java/hosting_support_backend/dto/response/WorkflowLogResponseDTO.java
+++ src/main/java/hosting_support_backend/dto/response/WorkflowLogResponseDTO.java
@@ -1,7 +1,15 @@
 package hosting_support_backend.dto.response;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WorkflowLogResponseDTO {
       private Long id;
     private String workflowName;
