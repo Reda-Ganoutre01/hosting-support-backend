1546b315af7128ffb2a4961e311397ede1e6d8d8
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 01:01:57 +0100

    fix: enhance security by updating JsonIgnoreProperties in HostingAccount entity

--- src/main/java/hosting_support_backend/entity/HostingAccount.java
+++ src/main/java/hosting_support_backend/entity/HostingAccount.java
@@ -1,6 +1,7 @@
 package hosting_support_backend.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import hosting_support_backend.entity.enums.HostingStatus;
 import jakarta.persistence.*;
@@ -31,7 +32,7 @@ public class HostingAccount {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
-    @JsonIgnoreProperties({"tickets", "hostingAccounts", "notifications"})
+    @JsonIgnoreProperties({"tickets", "hostingAccounts", "notifications", "password"})
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -39,5 +40,6 @@ public class HostingAccount {
     private HostingPlan hostingPlan;
 
     @OneToMany(mappedBy = "hostingAccount", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private java.util.List<Notification> notifications;
 }
