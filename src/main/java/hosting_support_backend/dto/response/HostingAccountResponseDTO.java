b88bf1b19136db8ef39047ed73ff1f936dcb17fa
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 01:01:34 +0100

    refactor: reorganize imports and formatting in HostingAccountResponseDTO

--- src/main/java/hosting_support_backend/dto/response/HostingAccountResponseDTO.java
+++ src/main/java/hosting_support_backend/dto/response/HostingAccountResponseDTO.java
@@ -1,16 +1,22 @@
 package hosting_support_backend.dto.response;
 
 import java.time.LocalDate;
-
 import hosting_support_backend.entity.enums.HostingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HostingAccountResponseDTO {
-  private Long id;
+    private Long id;
     private String domainName;
     private HostingStatus status;
     private LocalDate startDate;
     private LocalDate expirationDate;
     private Long userId;
     private Long hostingPlanId;
-
 }
