a254542a4a9a612f73cadfa58c0c97706002f6a2
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-16 01:01:41 +0100

    refactor: update formatting and annotations in TicketResponseDTO

--- src/main/java/hosting_support_backend/dto/response/TicketResponseDTO.java
+++ src/main/java/hosting_support_backend/dto/response/TicketResponseDTO.java
@@ -1,18 +1,23 @@
 package hosting_support_backend.dto.response;
 
 import java.time.LocalDateTime;
-
 import hosting_support_backend.entity.enums.Priority;
 import hosting_support_backend.entity.enums.TicketStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TicketResponseDTO {
-private Long id;
+    private Long id;
     private String subject;
     private String description;
     private TicketStatus status;
     private Priority priority;
     private LocalDateTime createdAt;
     private Long userId;
-
-
 }
