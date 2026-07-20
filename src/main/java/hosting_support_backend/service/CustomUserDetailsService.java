6603992300970b3167c2873ba260e1ded09ac288
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-20 20:25:17 +0100

    feat: implement CustomUserDetailsService for user authentication

---
+++ src/main/java/hosting_support_backend/service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package hosting_support_backend.service;
+
+import hosting_support_backend.entity.User;
+import hosting_support_backend.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    public CustomUserDetailsService (UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()->
+                        new UsernameNotFoundException("User not found:" + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUserName())
+                .password(user.getPassword())
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())))
+                .build();
+    }
+}
