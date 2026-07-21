7f7ff6892d11287e6a2d302c8d3b768a00b11b0b
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-07-21 21:01:15 +0100

    feat: add SecurityConstants for JWT expiration and secret configuration

---
+++ src/main/java/hosting_support_backend/constants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package hosting_support_backend.constants;
+
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION = 12 * 24 * 60 * 60 * 1000L;
+    public static final String JWT_SECRET = "acaca1497b7fdf6da8140aa9665c66528da21295e765d8a418b23b6990e1ca29";
+}
