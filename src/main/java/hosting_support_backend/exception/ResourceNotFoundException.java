85ffb3299945672cd8a54b52256223ed7ee673dd
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-09-01 18:39:57 +0100

    feat: add ResourceNotFoundException for handling resource not found errors

---
+++ src/main/java/hosting_support_backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package hosting_support_backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
