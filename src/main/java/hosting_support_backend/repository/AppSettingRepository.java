5d1d676be7072249955c34ddddffd744b8ea4542
Reda Gantt <121640970+Reda-Ganoutre01@users.noreply.github.com>
2026-09-11 20:58:44 +0100

    feat: create AppSettingRepository for accessing application settings

---
+++ src/main/java/hosting_support_backend/repository/AppSettingRepository.java
@@ -0,0 +1,10 @@
+package hosting_support_backend.repository;
+
+import hosting_support_backend.entity.AppSetting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppSettingRepository extends JpaRepository<AppSetting, Long> {
+    Optional<AppSetting> findBySettingKey(String settingKey);
+}
